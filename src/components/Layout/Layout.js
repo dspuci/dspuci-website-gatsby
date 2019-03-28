c260f740d99b8f84e526cfd4cf181dad8c91b04e
Farbod909 <rafezyfarbod@gmail.com>
2019-03-28 16:03:18 -0700

    Add basic footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,11 @@ import React from "react"
 import { NavbarSolid, NavbarTransparentWhite } from "../Navbar"
 import Home from "../Home"
 import { Container, MainContainer } from "../Container"
+import { lightColor, darkColor } from "../../styles/defaultColors"
+import { Flex, Box, Image } from "rebass"
+import uciLogo from "../../images/uci-logo.png"
+import phoneIcon from "../../images/phone.png"
+import emailIcon from "../../images/email.png"
 
 class Layout extends React.Component {
   constructor(props) {
@@ -45,9 +50,38 @@ class Layout extends React.Component {
         {this.state.navbar}
         {this.props.home ? <Home /> : null}
         {container}
+        <Footer />
       </div>
     )
   }
 }
+
+const Footer = props => (
+  <footer style={{ backgroundColor: darkColor }}>
+    <Container>
+      <Flex
+        color={lightColor}
+        fontSize={16}
+        flexWrap="wrap"
+        justifyContent="center"
+        style={{ textAlign: "center" }}
+      >
+        <Box mb={30} width={[1, 1 / 5]}>
+          <Image src={phoneIcon} height={15} />
+          <div>(310) 447-4011</div>
+        </Box>
+        <Box mb={30} width={[1, 1 / 5]}>
+          <Image src={emailIcon} height={15} />
+          <div>svp@dspuci.com</div>
+        </Box>
+        <Box mb={30} width={[1, 1 / 5]}>
+          <Image src={uciLogo} height={15} />
+          <div>Irvine, CA</div>
+        </Box>
+        <Box width={1}>©2019 Delta Sigma Pi - Pi Sigma Chapter</Box>
+      </Flex>
+    </Container>
+  </footer>
+)
 
 export default Layout
