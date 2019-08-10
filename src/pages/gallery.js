62e09ccaafd5d6b4698ca1b28c57e46b437dc764
Farbod909 <rafezyfarbod@gmail.com>
2019-08-10 12:05:16 -0700

    Show albums and album previews in gallery page

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,11 +1,35 @@
-import { withPrefix } from "gatsby"
+import { graphql, withPrefix } from "gatsby"
 import React from "react"
-import { Image } from "rebass"
+import { Flex, Image, Text } from "rebass"
 import { StandardLayout } from "../components/Layout"
 
-export default () => (
+export default ({ data }) => (
   <StandardLayout>
     <h1>Gallery</h1>
-    <Image src={withPrefix(`/images/brothers/winter19/farbod_rafezy.jpg`)} />
+    <Flex flexWrap="wrap">
+      {data.album_previews.group.map(album_preview => (
+        <div>
+          <Image
+            src={withPrefix(
+              `/images/gallery/${album_preview.nodes[0].preview_image_path}`
+            )}
+          />
+          <Text>{album_preview.nodes[0].album_name}</Text>
+        </div>
+      ))}
+    </Flex>
   </StandardLayout>
 )
+
+export const query = graphql`
+  {
+    album_previews: allFile(filter: { sourceInstanceName: { eq: "gallery" } }) {
+      group(field: dir, limit: 1) {
+        nodes {
+          album_name: relativeDirectory
+          preview_image_path: relativePath
+        }
+      }
+    }
+  }
+`
