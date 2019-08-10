6ebf9bd7abe547a204e71df3800cd6c8546d4cd7
Farbod909 <rafezyfarbod@gmail.com>
2019-08-09 23:42:58 -0700

    Dynamically add pages based on gallery folder

---
+++ src/templates/album.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { StandardLayout } from "../components/Layout"
+
+export default () => {
+  return (
+    <StandardLayout>
+      <h1>Album</h1>
+      <p>Album details go here</p>
+    </StandardLayout>
+  )
+}
