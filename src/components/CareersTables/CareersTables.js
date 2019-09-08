8ac594cf9f0f60c6d12df41107a2622010f8f633
Farbod909 <rafezyfarbod@gmail.com>
2019-09-08 13:57:01 -0700

    Add year picker to careers page

--- src/components/CareersTables/CareersTables.js
+++ src/components/CareersTables/CareersTables.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import CenterHeader from "../CenterHeader"
+import styles from "./CareersTables.module.css"
 
 const CareersTable = props => (
   <div>
@@ -14,18 +15,51 @@ const CareersTable = props => (
           <th>Company</th>
           <th>Location</th>
         </tr>
-        {props.children}
+        {props.data.nodes.map(node => (
+          <tr>
+            <td>{node.Name}</td>
+            <td>{node.Position}</td>
+            <td>{node.Company}</td>
+            <td>{node.Location}</td>
+          </tr>
+        ))}
       </table>
     </div>
   </div>
 )
 
+class YearButton extends React.Component {
+  handleClick = () => {
+    this.props.onYearButtonClick(this.props.value)
+  }
+
+  render() {
+    return (
+      <button
+        onClick={this.handleClick}
+        className={[
+          styles.button,
+          this.props.currentYear === this.props.value
+            ? styles.activeButton
+            : styles.inactiveButton,
+        ].join(" ")}
+      >
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
 class CareersTables extends React.Component {
   constructor() {
     super()
     this.state = {
-      year: 2019,
+      currentYear: "2019",
     }
+  }
+
+  handleYearButtonClick = year => {
+    this.setState({ currentYear: year })
   }
 
   render() {
@@ -33,7 +67,7 @@ class CareersTables extends React.Component {
       <StaticQuery
         query={graphql`
           {
-            internships: allCareersXlsxInternships2019 {
+            fullTimeOffers2020: allCareersXlsxFullTimeOffers2020 {
               nodes {
                 Name
                 Position
@@ -41,8 +75,15 @@ class CareersTables extends React.Component {
                 Location
               }
             }
-
-            fullTimeOffers: allCareersXlsxFullTimeOffers2019 {
+            internships2019: allCareersXlsxInternships2019 {
+              nodes {
+                Name
+                Position
+                Company
+                Location
+              }
+            }
+            fullTimeOffers2019: allCareersXlsxFullTimeOffers2019 {
               nodes {
                 Name
                 Position
@@ -52,30 +93,46 @@ class CareersTables extends React.Component {
             }
           }
         `}
-        render={data => (
-          <div>
-            <CareersTable title={`${this.state.year} Internships`}>
-              {data.internships.nodes.map(node => (
-                <tr>
-                  <td>{node.Name}</td>
-                  <td>{node.Position}</td>
-                  <td>{node.Company}</td>
-                  <td>{node.Location}</td>
-                </tr>
+        render={data => {
+          const careersData = {
+            "2019": [
+              {
+                name: "Internships",
+                data: data.internships2019,
+              },
+              {
+                name: "Full Time Offers",
+                data: data.fullTimeOffers2019,
+              },
+            ],
+            "2020": [
+              {
+                name: "Full Time Offers",
+                data: data.fullTimeOffers2020,
+              },
+            ],
+          }
+
+          return (
+            <div>
+              {Object.keys(careersData)
+                .reverse()
+                .map(key => (
+                  <YearButton
+                    value={key}
+                    currentYear={this.state.currentYear}
+                    onYearButtonClick={this.handleYearButtonClick}
+                  />
+                ))}
+              {careersData[this.state.currentYear].map(table => (
+                <CareersTable
+                  title={`${this.state.currentYear} ${table.name}`}
+                  data={table.data}
+                />
               ))}
-            </CareersTable>
-            <CareersTable title={`${this.state.year} Full Time Offers`}>
-              {data.fullTimeOffers.nodes.map(node => (
-                <tr>
-                  <td>{node.Name}</td>
-                  <td>{node.Position}</td>
-                  <td>{node.Company}</td>
-                  <td>{node.Location}</td>
-                </tr>
-              ))}
-            </CareersTable>
-          </div>
-        )}
+            </div>
+          )
+        }}
       />
     )
   }
