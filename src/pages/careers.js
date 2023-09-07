28f4b1c41921e26d47815141dca8f316ed51599e
Abigail Bareiss <abareiss@uci.edu>
2023-09-07 15:30:42 -0700

    Career page updates

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -30,7 +30,7 @@ export default ({ data }) => (
     <Section sx={{ justifyContent: "left" }}>
       <h1>A higher standard of career success.</h1>
       <div>
-        At UC Irvine, we’re redefining and raising the bar of successful
+        At UC Irvine, we are redefining and raising the bar of successful
         careers, and our experiences speak to our commitment to professional
         development. We have countless brothers with competitive internships and
         full-time experience at Fortune 500 companies, prestigious firms, and
